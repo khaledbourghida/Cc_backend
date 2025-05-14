e67fdc778c23ce8e6143ac8b7cb9311efec2f3cf
khaledbourghida <walidbourghida99@gmail.com>
2025-05-14 18:55:53 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getFunc, getCode, runCode} = require('../controllers/authController');
+const {getFunc, getCode, runCode , runFormat} = require('../controllers/authController');
 
 router.get('/functions/:type', getFunc);
 router.get('/functions/:type/:funcName', getCode);
 router.post('/run', runCode);
+router.post('/format' , runFormat);
+
 
 module.exports = router;
