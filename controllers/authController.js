5ca26e12b6eee88397c27f7de7e87cd685426229
khaledbourghida <walidbourghida99@gmail.com>
2025-05-15 00:44:55 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,6 @@ const path = require('path');
 const { exec } = require('child_process');
 const { promisify } = require('util');
 const execAsync = promisify(exec);
-const astyle = require('astyle');
-const cppLint = require('cpp-lint');
 const execFilePromise = util.promisify(execFile);
 
 
